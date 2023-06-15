d355b8a983da71bd3ad804837114223a85b7b379
Mikhail Malaschenko <malaschenko@itspartner.net>
2023-06-15 14:20:32 +0200

    Create project

---
+++ TestTableViewFromJsonApp/TestTableViewFromJsonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTableViewFromJsonApp
+//
+//  Created by Mikhail Malaschenko on 15.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
